57b443e51f07affa940dbbf8875488694ea17db3
cesar-a-jr <cesaraugustojunior1234@gmail.com>
2023-02-14 20:26:46 -0300

    responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Image from "next/image";
+import Head from "next/head";
+import eFront from '../assets/eFront.svg'
 
-import styles from '@/styles/Home.module.css'
-import Header from './components/header'
-
+import styles from "@/styles/Home.module.css";
+import Header from "./components/header";
 
 export default function Home() {
   return (
@@ -15,22 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Header/>
-        <div className={styles.width} >
-        <h1>Seja bem-vindo a sua melhor newsletter</h1>
-        <h2>Fique por dentro de todas a novidades</h2>
-        <p>Preencha os campos para receber as notícias sobre programação</p>
-        <div className={styles.input}>
-          <label htmlFor="">Nome</label>
-          <input type="text" name="" id="" />
-        </div>
-        <div className={styles.input}>
-          <label htmlFor="">E-mail</label>
-          <input type="text" name="" id="" />
-        </div>
-        <button className={styles.button}>Enviar</button>
+        <Header />
+        <div className={styles.width}>
+          <Image src={eFront} alt=''/>
+          <h1>Seja bem-vindo a sua melhor newsletter</h1>
+          <h2>Fique por dentro de todas a novidades</h2>
+          <p>Preencha os campos para receber as notícias sobre programação</p>
+          <div className={styles.input}>
+            <label htmlFor="">Nome</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className={styles.input}>
+            <label htmlFor="">E-mail</label>
+            <input type="text" name="" id="" />
+          </div>
+          <button className={styles.button}>Enviar</button>
         </div>
       </main>
     </>
-  )
+  );
 }
